4292e0b8abb0ae60a75923ee29f82170cddbcc8b
langlang149 <ouliang5@163.com>
2018-06-10 10:47:54 +0800

    文档修改

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 
 /*
-* VueAwesomeSwiper
-* Author: surmon@foxmail.com
-* Github: https://github.com/surmon-china/vue-awesome-swiper
+* VueAwesomeScroll
+* Author: ouliang5@163.com
+* Github: https://github.com/langlong149/vue-awesome-scroll
 */
-
 
 import _BScroll from 'better-scroll'
 import BScrollComponent from './Scroll.vue'
